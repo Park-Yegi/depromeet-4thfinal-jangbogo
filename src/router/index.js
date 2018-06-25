6bbb3121c6b85595527b1cf259820f648484b7ba
Park-Yegi <pyg9715@gmail.com>
2018-06-25 20:52:50 +0900

    Initial Setting

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,30 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Login from '@/components/Login'
+import Signup from '@/components/Signup'
+import Main from '@/components/Main'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      component: Main
+    },
+    {
+      path: '/login',
+      component: Login
+    },
+    {
+      path: '/signup',
+      component: Signup
+    },
+    {
+      path: '/main',
+      component: Main
     }
   ]
 })
